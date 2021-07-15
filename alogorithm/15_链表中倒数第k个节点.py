835ce99c90d2fff471bc8e453976f259549ec31b
xufeng010203 <guoyue12@hisilicon.com>
2021-07-15 23:17:40 +0800

    01

--- alogorithm/15_链表中倒数第k个节点.py
+++ alogorithm/15_链表中倒数第k个节点.py
@@ -1,30 +1,31 @@
-'''
+# '''
+#
+# 输入一个链表，输出该链表中倒数第k个节点
+#
+# '''
+#
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+#
+#
+#     def find_kth_o_tail(self, head, k):
+#         """
+#         查找链表中第k个节点
+#         :param k:
+#         :return:
+#         """
+#         if not head or not k:
+#             return None
+#         count = 0
+#         p = head
+#         q = head
+#         while count < k:
+#             p = p.next
+#             count += 1
+#         while p:
+#             q = q.next
+#             p = p.next
+#         return q.val
 
-输入一个链表，输出该链表中倒数第k个节点
-
-'''
-
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
-
-
-    def find_kth_o_tail(self, head, k):
-        """
-        查找链表中第k个节点
-        :param k:
-        :return:
-        """
-        if not head or not k:
-            return None
-        count = 0
-        p = head
-        q = head
-        while count < k:
-            p = p.next
-            count += 1
-        while p:
-            q = q.next
-            p = p.next
-        return q.val
